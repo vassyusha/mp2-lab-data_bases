1787e527ebd3d87d5bdda11e8413ddf2fbf0dc4f
vassyusha <romanoffmiss@yandex.ru>
2025-04-01 23:53:02 +0300

    operator++ for iterator done

--- include/RBTree.h
+++ include/RBTree.h
@@ -2,12 +2,30 @@
 #include <vector>
 #include <string>
 
+
+
 template<class TKey, class TValue>
-class unorderedTable {
+class RBTree {
 public:
 	class iterator;
+	enum Color {
+		RED,
+		BLACK
+	};
+
+protected:
+	struct Node {
+		int key;
+		Node* left, * right, * parent;
+		std::pair<TKey, TValue> value;
+		Color color; // RED or BLACK
+
+		Node(int key, Node* l, Node* r, Node p, std::pair<TKey, TValue> value, Color color): key(key), left(l), right(r), parent(p), value(value), color(color){}
+	};
+
 private:
-	std::vector<std::pair<TKey, TValue>> data;
+
+	Node* root;
 
 	void swap(iterator first, iterator second) {
 		std::pair<TKey, TValue> temp = *(first);
@@ -15,109 +33,142 @@ private:
 		*(second) = temp;
 	}
 
-public:
+	void smallLeftRotate(Node* x) {
+		Node* y = x->right;
 
-	size_t size() { return this->data.size(); }
+		//B - правое поддерево x
+		x->right = y->left;
+		if(y->left) y->left->parent = x;
+
+		//y - новый корень поддерева
+		y->parent = x->parent;
+		if (x->parent) {
+			if (x->parent->right == x) x->parent->right = y;
+			else x->parent->left = y;
+		}
+
+		//x - левое поддерево y
+		y->left = x;
+		x->parent = y;
+	}
+	void smallRightRotate(Node* x) {
+		Node* y = x->left;
+
+		//B - правое поддерево x
+		x->left = y->right;
+		if (y->right) y->right->parent = x;
+
+		//y - новый корень поддерева
+		y->parent = x->parent;
+		if (x->parent) {
+			if (x->parent->right == x) x->parent->right = y;
+			else x->parent->left = y;
+		}
+
+		//x - левое поддерево y
+		y->right = x;
+		x->parent = y;
+	}
+
+	void bigLeftRotate(Node* x) {
+		this->smallRightRotate(x->right);
+		this->smallLeftRotate(x);
+	}
+	void bigRightRotate(Node* x) {
+		this->smallLeftRotate(x->left);
+		this->smallRightRotate(x);
+	}
+
+	//relatives
+	Node* P(x) { //parent
+		return x->parent;
+	}
+	Node* G(x) { //granny
+		if (x->parent) return x->parent->parent;
+		return nullptr;
+	}
+	Node* U(x) { //uncle
+		if (x->parent && x->parent->parent) {
+			if (x->parent == x->parent->parent->right) return x->parent->parent->left;
+			return x->parent->parent->right;
+		}
+		return nullptr;
+	}
+	Node* S(x) { //sibling
+		if (x->parent) {
+			if (x == x->parent->right) return x->parent->left;
+			return x->parent->right;
+		}
+		return nullptr;
+	}
+
+public:
 
 	class iterator {
 	private:
-		std::pair<TKey, TValue>* it;
+		Node* it = nullptr;
 
 	public:
-		iterator(std::pair<TKey, TValue>* it): it(it){}
+		iterator(Node* it): it(it){}
 
-		std::pair<TKey, TValue>& operator*() { return *(this->it); }
-		std::pair<TKey, TValue> operator*() const { return *(this->it); }
+		Node& operator*() { return *(this->it); }
+		Node operator*() const { return *(this->it); }
 
+		bool operator!=(const iterator& other) const { return (this->it != other.it); }
+		bool operator==(const iterator& other) const { return !((*this) != other); }
+
+		//тут уже все переделывать надо, это анрил
 		iterator& operator++() {
-			this->it++;
-			return *this;
+			if (this->it->right) {
+				this->it = this->it->right;
+				while (this->it->left) this->it = this->it->left;
+				return (*this);
+			}
+			if (this->it->parent) {
+				while (this->it->parent && this->it != this->it->parent->left) this->it = this->it->parent;
+				if (this->it->parent && this->it == this->it->parent->left) {
+					this->it = this->it->parent;
+					return (*this);
+				}
+			}
+			while (this->it->right) this->it = this->it->right;
+			this->it = this->it->right;
+			return (*this);
 		}
 		iterator operator++(int) {
-			iterator i = *this;
+			iterator i = (*this);
 			++(*this);
 			return i;
 		}
 
-		bool operator!=(const iterator& other) const { return (this->it != other.it); }
-		bool operator==(const iterator& other) const { return !((*this) != other); }
-		bool operator>(const iterator& other) const { return (this->it > other.it); }
-		bool operator<(const iterator& other) const { return (this->it < other.it); }
-		bool operator>=(const iterator& other) const { return !((*this) < other); }
-		bool operator<=(const iterator& other) const { return !((*this) > other); }
+		iterator& operator--();
 
-		iterator& operator--() {
-			this->it--;
-			return *this;
-		}
-		iterator operator--(int) {
-			iterator i = *this;
-			--(*this);
-			return i;
-		}
 
-		iterator& operator+=(int n) {
-			this->it += n;
-			return *this;
-		}
-		iterator operator+(int n) const {
-			iterator i(this->it + n);
-			return i;
-		}
-
-		iterator& operator-=(int n) {
-			this->it -= n;
-			return *this;
-		}
-		iterator operator-(int n) const {
-			iterator i(this->it - n);
-			return i;
-		}
+		iterator operator--(int);
 
 	};
 
-	iterator begin() { return iterator(this->data.data()); }
-	iterator end() { return this->begin() + this->size(); }
-
-	iterator insert(const TKey& key, const TValue& value) {
-		if (this->find(key) != end()) throw "the element with this key already exists";
-		this->data.push_back(std::pair<TKey, TValue>(key, value));
-		return this->end()-1;
+	iterator begin() { 
+		Node* ll = this->root;
+		while (ll->left) ll = ll->left;
+		return iterator(ll);
+	}
+	iterator end() { 
+		Node* rr = this->root;
+		while (rr->right) rr = rr->right;
+		rr = rr->right;
+		return iterator(rr);
 	}
 
-	iterator find(const TKey& key) {
-		for (iterator it = this->begin(); it != this->end(); ++it) {
-			if ((*it).first == key) return it;
-		}
-		return this->end();
-	}
 
-	iterator erase(const iterator& it) {
-		if (it >= this->end()) throw "you are trying to erase a non-existant element";
-		swap(it, this->end() - 1);
-		this->data.pop_back();
-		return it;
-	}
-	iterator erase(const TKey& key) {
-		iterator it = this->find(key);
-		return this->erase(it);
-	}
+	iterator insert(const TKey& key, const TValue& value);
 
-	TValue& operator[](const TKey& key) {
-		if (this->find(key) == this->end()) {
-			this->data.push_back(std::pair<TKey, TValue>(key, TValue()));
-			return (*(this->end() - 1)).second;
-		}
-		return (*(this->find(key))).second; 
-	}
-	TValue operator[](const TKey& key) const {
-		if (this->find(key) == this->end()) throw "an element with such key doesnt exist";
-		return (*(this->find(key))).second;
-	}
+	iterator find(const TKey& key);
 
-	friend std::ostream& operator <<(std::ostream& ostr, unorderedTable& table) {
-		ostr << "|   KEY    |    VALUE   |\n";
-		for (iterator it = table.begin(); it != table.end(); ++it) ostr << "|       " << (*it).first << "       |       " << (*it).second << "      |\n";
-		return ostr;
-	}
-};
+	iterator erase(const iterator& it);
+	iterator erase(const TKey& key);
+
+	TValue& operator[](const TKey& key);
+	TValue operator[](const TKey& key) const;
+
+	friend std::ostream& operator <<(std::ostream& ostr, RBTree& tree);
